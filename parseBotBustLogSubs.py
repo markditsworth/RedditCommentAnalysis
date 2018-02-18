372954ee8b754a9e41f1add36ea2c73f55f3ae2a
Mark Ditsworth <mwditsworth@gmail.com>
2018-02-18 13:02:51 -0600

    Updated to ignore duplicates

--- parseBotBustLogSubs.py
+++ parseBotBustLogSubs.py
@@ -31,18 +31,21 @@ def sendToFile(dic, filename):
 
 def parse(filename_src,filename_dst):
     start = time.time()
-    sub_count = 0
+    bot_count = 0
+    unique_Names = []
     for item in iterateJSONObjects(filename_src):
         try:
             sub_info = json.loads(item)
             try:
                 if sub_info['subreddit'] == 'BotBustLog':
-                    sub_count = sub_count+1
                     botname = sub_info['title'].split(' ')[0]
                     if botname[0] == '/':
-                        with open(filename_dst, 'ab') as fObj:
-                            fObj.write(botname)
-                            fObj.write('\n')
+                        if botname not in unique_Names:
+                            bot_count = bot_count+1
+                            unique_Names.append(botname)
+                            with open(filename_dst, 'ab') as fObj:
+                                fObj.write(botname)
+                                fObj.write('\n')
             except KeyError:
                 pass
             
@@ -56,14 +59,14 @@ def parse(filename_src,filename_dst):
     duration_hour = duration/3600
     duration_min = (duration_hour - int(duration_hour))*60
     duration_sec = (duration_min - int(duration_min))*60
-    num = "{:,}".format(sub_count)
+    num = "{:,}".format(bot_count)
     print '%s bots found.'%num
     print 'Processing time: %d hours %d minutes %.2f seconds'%(duration_hour,duration_min,duration_sec)
-    #os.system('say "Program Finished"')
+    os.system('say "Program Finished"')
 
 if __name__ == '__main__':
-    file_name = 'RS_2016-11.txt'
-    file_name_dst = 'Nov2016_BotList.txt'
+    file_name = 'RS_2017-11.txt'
+    file_name_dst = 'Nov2017_BotList.txt'
     path = '/Volumes/My Passport for Mac/Grad/Networks and Systems'
     os.chdir(path)
     parse(file_name, file_name_dst)
