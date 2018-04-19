eb697e6ae025df63180afd3d6ca0e26c751ff5a4
Mark Ditsworth <mwditsworth@gmail.com>
2018-04-18 22:40:33 -0500

    removed sampling

--- plotting.py
+++ plotting.py
@@ -11,7 +11,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #%% DataFrame creation
-df = pd.read_pickle('../Stats/NetworkStatsDF.pkl')
+df = pd.read_pickle('../Stats/V2/NetworkStatsDF.pkl')
+#df = pd.read_pickle('../Stats/config/Config_NetworkStatsDF.pkl')
 df.drop(index='Node_For_Comments_On_Submission:',inplace=True)
 
 botlist = np.loadtxt('../Nov2017_BotList_update.txt',dtype=str)
@@ -20,35 +21,31 @@ dfBot = df.loc[botlist,:]
 
 #%% Plotting
 plt.figure(figsize=(12,8))
-plt.scatter(df['Out Degree'].values,df['Eigenvector'].values,s=1,c='k', label='All Accounts')
-plt.scatter(dfBot['Out Degree'].values,dfBot['Eigenvector'].values,s=30,marker='+',c='r',label='Bot Accounts')
-plt.xlabel('Out Degree Centrality')
-plt.ylabel('Eigenvector Centrality')
+plt.scatter(df['In Degree'].values,df['Clustering'].values,s=1,c='k', label='All Accounts')
+plt.scatter(dfBot['In Degree'].values,dfBot['Clustering'].values,s=30,marker='+',c='r',label='Bot Accounts')
+plt.xlabel('In Degree Centrality')
+plt.ylabel('Clustering Coefficient')
 plt.legend(loc='upper right')
 plt.show()
 
 #%% Sampling
 #interesting_accts = df[(df['Clustering']<0.0018) & (df['In Degree']>250)].index.values
 #interesting_accts = df[df['Clustering']>0.03].index.values
-#interesting_accts = df[(df['Clustering']<0.0018) & (df['Out Degree']>250)].index.values
-
-drop_accts = df[df['Clustering']>=0.0018].index.values
-selected_accts = df[df['Clustering']<0.0018].index.values
+interesting_accts = df[(df['Clustering']<0.0018) & (df['Out Degree']>250)].index.values
 
 # For getting interesting_accts that are NOT in botlist
-#interesting_accts1 = np.setdiff1d(interesting_accts,botlist,assume_unique=True)
+interesting_accts1 = np.setdiff1d(interesting_accts,botlist,assume_unique=True)
 # For getting interesting_accts that ARE in botlist
 #interesting_accts1 = np.intersect1d(interesting_accts,botlist)
 
-#drop_list = np.setdiff1d(df.index.values,selected_accts,assume_unique=True)
 
-#print len(interesting_accts)
-#print len(interesting_accts1)
-print len(selected_accts)
-for acct in drop_accts:
-    with open('Droplist_for_sample.txt','ab') as fObj:
-        #fObj.write('www.reddit.com/u/'+acct+'\n')
-        fObj.write(acct+'\n')
+print len(interesting_accts)
+print len(interesting_accts1)
+
+for acct in interesting_accts1:
+    with open('Check_These_Clustering_Out.txt','ab') as fObj:
+        fObj.write('www.reddit.com/u/'+acct+'\n')
+        #fObj.write(acct+'\n')
 
 
 print 'Done.'
